52e04dfb3b50a937ffaa6a1e15e687840cd85e90
SangDoo Nam <kexe009@hotmail.com>
2023-11-28 17:52:29 +0100

    finish

--- src/app.py
+++ src/app.py
@@ -3,9 +3,41 @@ from typing import Union, Callable
 
 
 def compare_to_seven(input: Union[int, str, Callable]) -> str:
-    pass
+    
     # TODO: write function
-
-
+    if isinstance(input, int):
+        if input > 7:
+            return f'{input} is higher than 7.'
+        elif input == 7:
+            return f"{input} is 7."
+        else:
+            return f'{input} is lower than 7.'
+    elif isinstance(input, str) and input.isnumeric():
+        if  int(input) > 7:
+            return f'{input} is higher than 7.'
+        elif int(input) == 7:
+            return f"{input} is 7."
+        else:
+            return f'{input} is lower than 7.'
+    elif isinstance(input, str) and not input.isnumeric():
+        try:
+            if ord(input) > 7:
+                return f'{input} is higher than 7.'
+            elif ord(input) == 7:
+                return f"{input} is 7."
+            else:
+                return f'{input} is lower than 7.'
+        except TypeError:
+            if len(input) > 7:
+                return f'{input} is higher than 7.'
+            elif len(input) == 7:
+                return f"{input} is 7."
+            else:
+                return f'{input} is lower than 7.'
+    elif isinstance(input, Callable):
+        return 'It is callable'
+    
+    
+    
 if __name__ == "__main__":
     compare_to_seven(8)
