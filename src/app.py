0b613c6341d63fec410ce264fcc542e09b2b575c
SangDoo Nam <kexe009@hotmail.com>
2023-11-29 13:59:50 +0100

    modify after solution

--- src/app.py
+++ src/app.py
@@ -2,41 +2,63 @@
 from typing import Union, Callable
 
 
-def compare_to_seven(input: Union[int, str, Callable]) -> str:
+# def compare_to_seven(input: Union[int, str, Callable]) -> str:
     
+#     # TODO: write function
+#     if isinstance(input, int):
+#         if input > 7:
+#             return f'{input} is higher than 7.'
+#         elif input == 7:
+#             return f"{input} is 7."
+#         else:
+#             return f'{input} is lower than 7.'
+#     elif isinstance(input, str) and input.isnumeric():
+#         if  int(input) > 7:
+#             return f'{input} is higher than 7.'
+#         elif int(input) == 7:
+#             return f"{input} is 7."
+#         else:
+#             return f'{input} is lower than 7.'
+#     elif isinstance(input, str) and not input.isnumeric():
+#         try:
+#             if ord(input) > 7:
+#                 return f'{input} is higher than 7.'
+#             elif ord(input) == 7:
+#                 return f"{input} is 7."
+#             else:
+#                 return f'{input} is lower than 7.'
+#         except TypeError:
+#             if len(input) > 7:
+#                 return f'{input} is higher than 7.'
+#             elif len(input) == 7:
+#                 return f"{input} is 7."
+#             else:
+#                 return f'{input} is lower than 7.'
+#     elif isinstance(input, Callable):
+#         return 'It is callable'
+
+
+def compare_to_seven(input: Union[int, str, Callable]) -> str:
+    num = None
     # TODO: write function
     if isinstance(input, int):
-        if input > 7:
-            return f'{input} is higher than 7.'
-        elif input == 7:
-            return f"{input} is 7."
-        else:
-            return f'{input} is lower than 7.'
+        num = input
     elif isinstance(input, str) and input.isnumeric():
-        if  int(input) > 7:
-            return f'{input} is higher than 7.'
-        elif int(input) == 7:
-            return f"{input} is 7."
-        else:
-            return f'{input} is lower than 7.'
+        num = int(input)
     elif isinstance(input, str) and not input.isnumeric():
         try:
-            if ord(input) > 7:
-                return f'{input} is higher than 7.'
-            elif ord(input) == 7:
-                return f"{input} is 7."
-            else:
-                return f'{input} is lower than 7.'
+            num = ord(input)
         except TypeError:
-            if len(input) > 7:
-                return f'{input} is higher than 7.'
-            elif len(input) == 7:
-                return f"{input} is 7."
-            else:
-                return f'{input} is lower than 7.'
+            num = len(input)
     elif isinstance(input, Callable):
         return 'It is callable'
     
+    if num > 7:
+            return f'{input} is higher than 7.'
+    elif num == 7:
+        return f"{input} is 7."
+    else:
+        return f'{input} is lower than 7.'
     
     
 if __name__ == "__main__":
